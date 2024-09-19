9dfb42daf3ea543a12d782ebd64fe8765e4c9efd
SardarWahab <wahabsardar130@gmail.com>
2024-09-20 00:08:16 +0500

    mycom

--- pin/views.py
+++ pin/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login , authenticate
 # Create your views here.
 def handle_login(request):
     if request.method == 'POST':
-        print('i got form data')
+        # print('i got form data')
         username = request.POST.get('username')
         password = request.POST.get('password')
         new_user = authenticate(request, username=username,password=password)
@@ -22,6 +22,12 @@ def handle_register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
+        exesting_username = User.objects.filter(username=username).first()
+        if exesting_username:
+            return HttpResponse('username already exist')
+        exesting_email = User.objects.filter(email=email).first()
+        if exesting_email:
+            return HttpResponse('Email already exist')
         # print("i got submitted data",username,email,password)
         new_user = User.objects.create(username=username,email=email,password=password)
         new_user.save()
