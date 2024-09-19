32fc238508d660324ae35dec9477fec6b1cf18e4
SardarWahab <wahabsardar130@gmail.com>
2024-09-19 23:59:52 +0500

    mycom

---
+++ pin/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login , authenticate
+# Create your views here.
+def handle_login(request):
+    if request.method == 'POST':
+        print('i got form data')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        new_user = authenticate(request, username=username,password=password)
+        if new_user:
+            login(request,new_user)
+    else:
+        print('no form submitted')
+
+    return redirect('home')
+
+
+def handle_register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # print("i got submitted data",username,email,password)
+        new_user = User.objects.create(username=username,email=email,password=password)
+        new_user.save()
+    
+    return redirect('home')
--- pinterest/urls.py
+++ pinterest/urls.py
@@ -2,10 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from pin import views as app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home),
+    path('',home, name='home'),
     path('create/',create),
-    path('profile/',profile)
+    path('profile/',profile),
+    path('login/',app_views.handle_login,name='login'),
+    path('register/',app_views.handle_register,name='register')
 ]
