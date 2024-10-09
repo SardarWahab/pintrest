723e89172ae91d8e26fc66dbfd935d9f6eac629c
SardarWahab <wahabsardar130@gmail.com>
2024-10-09 21:31:24 +0500

    mycom

--- pin/models.py
+++ pin/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 # Create your models here.
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=13, blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile/',blank=True, null=True)
+    DOB = models.DateField()
+
 
 class Pin(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
